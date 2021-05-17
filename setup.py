42b1bff5f805222fcd8e3d5937c18421ff18f076
Masaharu Kato <fudai.mk@gmail.com>
2021-05-17 15:13:49 +0900

    use setup.cfg

--- setup.py
+++ setup.py
@@ -1,28 +1,9 @@
-from glob import glob
-from os.path import basename
-from os.path import splitext
-
 from setuptools import setup
-from setuptools import find_packages
+setup()
 
 
-def _requires_from_file(filename):
-    return open(filename).read().splitlines()
-
-
-setup(
-    name='pip-pytest-template',
-    version='0.1.0',
-    license="ライセンス",
-    description='Python project template with pip and pytest',
-    author="作成者",
-    url='https://github.com/masaharu-kato/pip-pytest-template',
-    packages=find_packages('src'),
-    package_dir={'': 'src'},
-    py_modules=[splitext(basename(path))[0] for path in glob('src/*.py')],
-    include_package_data=True,
-    zip_safe=False,
-    install_requires=_requires_from_file('requirements.txt'),
-    setup_requires=['pytest-runner'],
-    tests_require=['pytest', 'pytest-cov']
-)
+# setup(
+#     packages=find_packages('src'),
+#     package_dir={'': 'src'},
+#     py_modules=[splitext(basename(path))[0] for path in glob('src/*.py')],
+# )
