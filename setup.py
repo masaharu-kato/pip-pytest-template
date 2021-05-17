1a9bf74710e9f270385dbdeac745f45b7658e436
Masaharu Kato <fudai.mk@gmail.com>
2021-05-17 16:26:28 +0900

    remove comments

--- setup.py
+++ setup.py
@@ -1,9 +1,2 @@
 from setuptools import setup
 setup()
-
-
-# setup(
-#     packages=find_packages('src'),
-#     package_dir={'': 'src'},
-#     py_modules=[splitext(basename(path))[0] for path in glob('src/*.py')],
-# )
